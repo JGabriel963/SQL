5cd3ff635a63f1b34b080ffa530096c333bfae09
João Gabriel <joaogabriel9633@gmail.com>
2023-09-05 19:52:42 -0300

    Iniciando projeto novamente

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import { View, Text, StyleSheet } from "react-native";
-
-import { NavigationContainer } from '@react-navigation/native'
-import { Routes } from "./src/routes";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Routes/>
-    </NavigationContainer>
-  )
+    <View style={styles.container}>
+      <Text>Hello, World!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

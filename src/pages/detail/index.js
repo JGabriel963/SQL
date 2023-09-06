7b2b12bfb954f9d89a118333adff105a42c84f92
João Gabriel <joaogabriel9633@gmail.com>
2023-09-06 10:04:41 -0300

    Lesson two done

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,9 +1,31 @@
-import { View, Text, StyleSheet } from "react-native";
+import { useLayoutEffect } from "react";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { useRoute, useNavigation } from "@react-navigation/native";
+
+import { Entypo } from "@expo/vector-icons"
 
 export default function Detail() {
+    const route = useRoute();
+    const navigation = useNavigation()
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: route.params?.data ? route.params?.data.name : "Detalhe da receita",
+            headerRight: () => (
+                <Pressable >
+                    <Entypo 
+                    name="heart"
+                    size={28}
+                    color="#ff4141"
+                />
+                </Pressable>
+            )
+        })
+    }, [navigation, route.params?.data])
+
     return(
         <View style={styles.continer}>
-            <Text>Página Detalhes da receita</Text>
+            <Text>{route.params?.data.name}</Text>
         </View>
     )
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,72 +1,97 @@
-import { View, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 import { Logo } from "../../components/logo";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import api from "../../services/app";
+import FoodList from "../../components/foodlist";
 
 export default function Home() {
-    const [inputValue, setInputValue] = useState("")
+  const [inputValue, setInputValue] = useState("");
+  const [foods, setFoods] = useState([]);
 
-    function handleSearch() {
-        alert("Você digitou: " + inputValue)
+  useEffect(() => {
+    async function fetchApi() {
+      const response = await api.get("/foods");
+      setFoods(response.data);
     }
 
+    fetchApi();
+  }, []);
 
-    return(
-        <SafeAreaView style={styles.continer}>
-            <Logo/>
+  function handleSearch() {
+    alert("Você digitou: " + inputValue);
+  }
 
-            <Text style={styles.title}>Encontre a receita</Text>
-            <Text style={styles.title}>que combine com você</Text>
+  return (
+    <SafeAreaView style={styles.continer}>
+      <Logo />
 
-            <View style={styles.form}>
-                <TextInput
-                    value={inputValue}
-                    onChangeText={(text) => setInputValue(text)}
-                    placeholder="Digite o nome da comida..."
-                    style={styles.input}
-                />
+      <Text style={styles.title}>Encontre a receita</Text>
+      <Text style={styles.title}>que combine com você</Text>
 
-                <TouchableOpacity 
-                    onPress={handleSearch}
-                >
-                    <Ionicons name="search" size={28} color="#4cbe6c" />
-                </TouchableOpacity>
-            </View>
-        </SafeAreaView>
-    )
+      <View style={styles.form}>
+        <TextInput
+          value={inputValue}
+          onChangeText={(text) => setInputValue(text)}
+          placeholder="Digite o nome da comida..."
+          style={styles.input}
+        />
+
+        <TouchableOpacity onPress={handleSearch}>
+          <Ionicons name="search" size={28} color="#4cbe6c" />
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={foods}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => <FoodList data={item} />}
+        showsVerticalScrollIndicator={false}
+      />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    continer: {
-        flex: 1,
-        backgroundColor: "#f3f9ff",
-        paddingTop: 36,
-        paddingStart: 14,
-        paddingEnd:14
-    },
-    title: {
-        fontSize: 26,
-        fontWeight: "bold",
-        color: "#0e0e0e"
-    },
-    form: {
-        backgroundColor: "#fff",
-        width: "100%",
-        borderRadius: 8,
-        marginTop: 16,
-        marginBottom: 16,
-        borderWidth: 1,
-        borderColor: "#ececec",
-        paddingLeft: 8,
-        paddingRight: 8,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center"
-    },
-    input: {
-        width: "90%",
-        maxWidth: "90%",
-        height: 54
-    }
-})
+  continer: {
+    flex: 1,
+    backgroundColor: "#f3f9ff",
+    paddingTop: 20,
+    paddingStart: 14,
+    paddingEnd: 14,
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: "bold",
+    color: "#0e0e0e",
+  },
+  form: {
+    backgroundColor: "#fff",
+    width: "100%",
+    borderRadius: 8,
+    marginTop: 16,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: "#ececec",
+    paddingLeft: 8,
+    paddingRight: 8,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "90%",
+    maxWidth: "90%",
+    height: 54,
+  },
+});

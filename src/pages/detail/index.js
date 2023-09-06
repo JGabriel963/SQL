0c518fcd5d1787488d4ef1398689b49997e94215
João Gabriel <joaogabriel9633@gmail.com>
2023-09-06 20:39:46 -0300

    Teste APK

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,8 +1,8 @@
 import { useLayoutEffect } from "react";
-import { View, Text, StyleSheet, Pressable } from "react-native";
+import { View, Text, StyleSheet, Pressable, ScrollView, Image } from "react-native";
 import { useRoute, useNavigation } from "@react-navigation/native";
 
-import { Entypo } from "@expo/vector-icons"
+import { Entypo, AntDesign } from "@expo/vector-icons"
 
 export default function Detail() {
     const route = useRoute();
@@ -24,14 +24,47 @@ export default function Detail() {
     }, [navigation, route.params?.data])
 
     return(
-        <View style={styles.continer}>
-            <Text>{route.params?.data.name}</Text>
-        </View>
+        <ScrollView style={styles.continer} showsVerticalScrollIndicator={false} >
+           <Pressable>
+            <View style={styles.playIcon}>
+                <AntDesign name="playcircleo" size={44} color="#fafafa" />
+            </View>
+            <Image 
+                source={{ uri: route.params?.data.cover }}
+                style={styles.cover}
+            />
+           </Pressable>
+
+           <View style={styles.headerDetails}>
+                <View>
+                    <Text>{route.params?.data.name}</Text>
+                    <Text></Text>
+                </View>
+           </View>
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
     continer: {
-        backgroundColor: 'yellow'
+        backgroundColor: '#f3f9ff',
+        paddingTop: 14,
+        paddingEnd: 14,
+        paddingStart: 14
+    },
+    cover: {
+        height: 200,
+        borderRadius: 14,
+        width: "100%"
+    },
+    playIcon: {
+        position: "absolute",
+        zIndex: 9,
+        top: 0, left: 0, right: 0, bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    headerDetails: {
+
     }
 })
